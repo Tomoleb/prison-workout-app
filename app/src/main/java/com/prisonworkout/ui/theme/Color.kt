c577dd06c2af4b856107ee7891fe65b50647c4da
Tomoleb <149542007+Tomoleb@users.noreply.github.com>
2026-05-06 00:26:02 +0300

    Add color definitions for theme

---
+++ app/src/main/java/com/prisonworkout/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.prisonworkout.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange500 = Color(0xFFFF9800)
+val Orange700 = Color(0xFFF57C00)
+val DarkBackground = Color(0xFF121212)
+val Surface = Color(0xFF1E1E1E)
+val OnSurface = Color.White
